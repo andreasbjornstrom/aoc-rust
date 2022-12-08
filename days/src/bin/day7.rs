8e7f6a08ad98fa929e6a5bc26913ef3a8bae67fc
Andreas Bjornstrom <andreas.bjornstrom@r2m.se>
2022-12-08 20:38:57 +0100

    part 2

--- days/src/bin/day7.rs
+++ days/src/bin/day7.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::iter::Map;
+use std::ops::Sub;
 
 fn main() {
     let data = "$ cd /
@@ -26,24 +27,16 @@ $ ls
 5626152 d.ext
 7214296 k";
     //let day = 0;
-    //let data = aoc::get_aoc_file(7);
-    let sum_part1: usize = parse_things_part1(&data);
-    let sum_part2: usize = data.split("\n").map(parse_things_part2).sum();
+    let data = aoc::get_aoc_file(7);
+    let sum: HashMap<String, usize> = parse_things(&data);
+
+    let sum_part1: usize = part_1(&sum);
+    let sum_part2: usize = part_2(&sum);
     println!("part 1 {:?}", sum_part1);
     println!("part 2 {:?}", sum_part2)
 }
 
-fn parse_things_part2(row: &str) -> usize {
-    if row.is_empty() {
-        return 0;
-    }
-    return 0;
-}
-
-fn parse_things_part1(row: &str) -> usize {
-    if row.is_empty() {
-        return 0;
-    }
+fn parse_things(row: &str) -> HashMap<String, usize> {
     let mut current_dir: Vec<&str> = Vec::new();
     let mut dir_content: HashMap<String, Vec<(usize, String)>> = HashMap::new();
     for x in row.lines() {
@@ -85,7 +78,7 @@ fn parse_things_part1(row: &str) -> usize {
         }
     }
 
-    let mut folder_size: HashMap<&String, usize> = HashMap::new();
+    let mut folder_size: HashMap<String, usize> = HashMap::new();
     for folder in &dir_content {
         let mut total = 0;
         for file in folder.1 {
@@ -94,12 +87,29 @@ fn parse_things_part1(row: &str) -> usize {
             }
         }
         let size = folder.1.iter().map(|x| x.0).sum::<usize>();
-        folder_size.insert(folder.0, size + total);
+        folder_size.insert(folder.0.clone(), size + total);
     }
 
+    return folder_size;
+}
+fn part_2(folder_size: &HashMap<String, usize>) -> usize {
+    let available_disk = 70_000_000 as usize;
+    let unused_needed = 30_000_000 as usize;
+    let root_usage = folder_size.get(&"/".to_string()).unwrap();
+    let min_size_to_delete = unused_needed.sub(available_disk.sub(root_usage));
+    let mut deleteable_folder = folder_size
+        .iter()
+        .filter(|(_, v)| v.ge(&&min_size_to_delete))
+        .map(|(_, v)| v)
+        .collect::<Vec<&usize>>();
+    deleteable_folder.sort();
+    return **deleteable_folder.first().unwrap();
+}
+
+fn part_1(folder_size: &HashMap<String, usize>) -> usize {
     let mut total: usize = 0;
     for folder in folder_size {
-        if folder.1 <= 100_000 {
+        if folder.1 <= &100_000 {
             total += folder.1
         }
     }
@@ -109,7 +119,7 @@ fn parse_things_part1(row: &str) -> usize {
 
 fn get_folder_size(p0: &HashMap<String, Vec<(usize, String)>>, p1: &String) -> usize {
     let mut total = 0;
-    for (size, file) in p0.get(p1).unwrap() {
+    for (size, file) in p0.get(&*p1).unwrap() {
         if size == &0 {
             total += get_folder_size(p0, file)
         }
